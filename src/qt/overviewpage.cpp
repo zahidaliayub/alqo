7cfb326696549e18a9ade4c45af7de6da443e7cf
Kevin Collmer <kevin.collmer@googlemail.com>
2018-04-09 01:54:51 +0200

    hide darksend features

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -118,6 +118,8 @@ OverviewPage::OverviewPage(QWidget* parent) : QWidget(parent),
     nDisplayUnit = 0; // just make sure it's not unitialized
     ui->setupUi(this);
 
+	ui->frameDarksend->setVisible(false);
+	
     // Recent transactions
     ui->listTransactions->setItemDelegate(txdelegate);
     ui->listTransactions->setIconSize(QSize(DECORATION_SIZE, DECORATION_SIZE));
